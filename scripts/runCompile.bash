5226db851da3011de9f61afddc25eaa8d04873aa
Oliver Dietzel <oliver-pierre.dietzel@uni-jena.de>
2026-02-28 17:30:56 +0100

    Update runCompile.bash to support OS-specific paths and simplify argument handling

--- scripts/runCompile.bash
+++ scripts/runCompile.bash
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 2 ]; then
-    echo "Verwendung: $0 <arg1> <arg2>"
+if [ $# -lt 1 ]; then
+    echo "Verwendung: $0 <arg1>"
     exit 1
 fi
 
+# Betriebssystem erkennen
+if [[ "$(uname)" == "Darwin" ]]; then
+    CIVAS="./test/compile/vm/macos/civas"
+    CIVVM="./test/compile/vm/macos/civvm"
+else
+    CIVAS="./build/vm/linux/civas"
+    CIVVM="./build/vm/linux/civvm"
+fi
+
 echo "Kompilieren..."
-make -C build || exit 1
+make -C build  || exit 1
 
 echo "Run civicc"
-./build/civicc $1 -o $2 || exit 1
+./build/civicc $1 -o test/compile/out || exit 1
 
 echo "Run civas"
-./test/compile/vm/civas $2 || exit 1
+$CIVAS test/compile/out || exit 1
 
 echo "Run civvm"
-./test/compile/vm/civvm civ.out || exit 1
+$CIVVM civ.out  || exit 1
